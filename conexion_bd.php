9099ffa62b67b5d463d4ef3cd805bac3ba75722b
Raymundo Gutiérrez <129213631+RaymundoGtz@users.noreply.github.com>
2023-12-05 07:19:29 -0500

    Delete conexion_bd.php

--- conexion_bd.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $username = "root"; 
-    $password = ""; 
-    $database = "mq7_data"; 
-
-    $conn = mysqli_connect($hostname, $username, $password, $database);
-?>
